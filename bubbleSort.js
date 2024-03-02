080b0a59870e9407821967f3e9c575697d2faab9
unknown <ohidujjamanfahad987@gmail.com'git config --global user.name fahad>
2024-03-03 00:57:43 +0600

    selection sort

---
+++ bubbleSort.js
@@ -0,0 +1,47 @@
+//one way to do this
+// function bubbleSort(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         //SWAP
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+// function bubbleSort(arr) {
+//   for (let i = arr.length; i > 0; i--) {
+//     for (let j = 0; j < i - 1; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         //SWAP
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+//optimization for already sorted arrays
+function bubbleSort(arr) {
+  var noSwaps;
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        //SWAP
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;
+      }
+    }
+  }
+  return arr;
+}
+console.log(bubbleSort([2, 6, 9, 2, 1, 8, 5, 6, 3]));
---
+++ selectionSort.js
@@ -0,0 +1,38 @@
+//selection sort pseudocode
+/**
+ * 1 ) store the first element as the smallest value you've seen so far.
+ * 2 ) compare this item to the next item in the array until you find a smaller number
+ * 3 ) if a smaller number is found designated the smaller number to be the new minimum and continue until the end of the array
+ * 4 ) if the minimum is not the value (index) you initially began with swap the two values
+ * 5 ) repeat this with the next element until the array is sorted.
+ */
+
+function selectionSort(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        var lowest = i;
+        console.log('arr[i]: ', arr[i]);
+        console.log('lowest .. i', lowest)
+        for (var j = i + 1; j < arr.length; j++) {
+            console.log('arr[j]', arr[j]);
+            if (arr[j] < arr[lowest]) {
+                console.log('arr[lowest]', arr[lowest]);
+                lowest = j;
+                console.log('lowest', lowest)
+            }
+        }
+        // console.log('****************')
+        // console.log('before swapping arr', arr);
+        // console.log('swapping to:');
+        if (i !== lowest) {
+            var temp = arr[i];
+            arr[i] = arr[lowest];
+            arr[lowest] = temp;
+        }
+
+        // console.log('after swapping arr', arr);
+        // console.log('****************')
+    }
+    return arr;
+}
+
+selectionSort([34, 22, 10, 19, 17])
